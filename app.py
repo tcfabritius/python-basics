350e332735b2f3d8de81f541ac49905ab2dc27f9
tcfabritius <107303668+tcfabritius@users.noreply.github.com>
2023-02-10 13:35:23 +0200

    Update app.py
    - More parts.
    - Edit and view-only modes.

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
 from math import *
 
+# DO YOU WANNA EDIT OR JUST VIEW EXAMPLES BELOW?
+editing = False
+
 # 1 Print pattern
 def part1():
     print('   /|')
@@ -129,5 +132,99 @@ def part11():
     print("Bottom")
     print()
 
-# TESTING
-part11()
+# 12 Return statements
+def part12():
+    def cube(num):
+        return num*num*num
+        print("not visible after return.")
+    rresult = cube(4)
+    print(rresult)
+    print()
+
+# 13 If statements
+def part13():
+    is_male = True
+    is_tall = False
+    if is_male and is_tall:
+        print("You are a tall male.")
+    elif is_male and not(is_tall):
+        print("You are a short male.")
+    elif not(is_male) and is_tall:
+        print("You are not a male but you are tall.")
+    else:
+        print("You are neither male or tall or both.")
+
+    print()
+
+# 14 If Comparisons
+def part14():
+    def max_num(inum1, inum2, inum3):
+        if inum1 >= inum2 and inum1 >= inum3:
+            return inum1
+        elif inum2 >= inum1 and inum2 >= inum3:
+            return inum2
+        else:
+            return inum3
+
+    print(max_num(4, 7, 2))
+    def cstring(string1, string2):
+        if string1 != string2:
+            return "Strings do not match"
+        else:
+            return "Strings match"
+    print(cstring("Halo", "Halo"))
+    print()
+
+# APP START HERE
+if editing:
+    # TESTING IN EDITING MODE. PUT PART NUMBER BELOW.
+    part14()
+
+else:
+    # ONLY RUNNING MODE WITH INPUTS.
+    print("WELCOME!")
+    print("1. Print")
+    print("2. Story with variables")
+    print("3. String basics")
+    print("4. Number basics")
+    print("5. Inputs")
+    print("6. Calculator")
+    print("7. Mad Libs Game")
+    print("8. List Basics")
+    print("9. List Functions")
+    print("10. Tuples on list")
+    print("11. Functions")
+    print("12. Return statements")
+    print("13. If statements")
+    print("14. If comparisons")
+    selectnum = input("Select part number from list: ")
+    if selectnum == "1":
+        part1()
+    elif selectnum == "2":
+        part2()
+    elif selectnum == "3":
+        part3()
+    elif selectnum == "4":
+        part4()
+    elif selectnum == "5":
+        part5()
+    elif selectnum == "6":
+        part6()
+    elif selectnum == "7":
+        part7()
+    elif selectnum == "8":
+        part8()
+    elif selectnum == "9":
+        part9()
+    elif selectnum == "10":
+        part10()
+    elif selectnum == "11":
+        part11()
+    elif selectnum == "12":
+        part12()
+    elif selectnum == "13":
+        part13()
+    elif selectnum == "14":
+        part14()
+    else:
+        print("ERROR INVALID INPUT.")
