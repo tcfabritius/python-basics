465b7a56db530dc6281d1f424085fa279def66fb
tcfabritius <107303668+tcfabritius@users.noreply.github.com>
2023-02-17 15:20:36 +0200

    Create Chef.py

---
+++ Chef.py
@@ -0,0 +1,10 @@
+class Chef:
+
+    def make_chicken(self):
+        print("Chef makes a chicken")
+
+    def make_salad(self):
+        print("Chef makes a salad")
+
+    def make_special_dish(self):
+        print("Chef makes bbq ribs")
